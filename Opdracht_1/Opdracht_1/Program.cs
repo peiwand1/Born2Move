3818b5503b4ddc2b0c8c27c271c4c19ce1745209
Peiwand Ismaiel <peiwand@hotmail.nl>
2024-06-18 16:58:28 +0200

    added user input

--- Opdracht_1/Opdracht_1/Program.cs
+++ Opdracht_1/Opdracht_1/Program.cs
@@ -6,10 +6,18 @@ namespace Sort_Opdracht
     {
         static void Main(string[] args)
         {
-            int count = 10;
+            Console.WriteLine("How many numbers to generate:");
+            int count;
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out count))
+            {
+                count = 10;
+            }
             List<int> nrList = getRandomNumbers(-99, 99, count);
             //List<int> nrList = new List<int>([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
             Console.WriteLine("\n\nSorting...\n");
+
+            //TODO test hoe lang deze erover doen met stopwatch
 
             //ShiftHighestSort shiftHighestSort = new ShiftHighestSort();
             //nrList = shiftHighestSort.Sort(nrList);
@@ -20,9 +28,10 @@ namespace Sort_Opdracht
             RotateSort rotateSort = new RotateSort();
             nrList = rotateSort.Sort(nrList);
 
-            foreach (int i in nrList)
+            for (int i = 0; i < nrList.Count; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(nrList[i]);
+                if (i >= 200) break;
             }
 
             Console.WriteLine(validateSort(nrList, count) ? "sort successful" : "sort unsuccessful");
