eab130db57f4d2d88711545326df457f2c880fd0
Peiwand Ismaiel <peiwand@hotmail.nl>
2024-06-19 10:46:43 +0200

    Update RotateSort.cs

--- Opdracht_1/Opdracht_1/RotateSort.cs
+++ Opdracht_1/Opdracht_1/RotateSort.cs
@@ -29,24 +29,22 @@ namespace Sort_opdracht
             array = Partitioning(array.GetRange(start, end - start));
         }
 
-        // recursive function for sorting
         private List<int> Partitioning(List<int> partition)
         {
-            // immediately return if list is 1 element or smaller
-            // this stops further recursion when it doesn't serve a purpose
+            // immediately return if list is 1 element or smaller to prevent infinite recursion
             if (partition.Count <= 1) return partition;
 
             // create 2 lists, 1 for values smaller than pivot, one for values bigger or equal to pivot
             List<int> prePivot = new List<int>();
             List<int> postPivot = new List<int>();
-            int pivotVal = rnd.Next(0, partition.Count); // choose a random number as pivot
+            int pivotIndex = rnd.Next(0, partition.Count); // choose a random number as pivot
 
             // loop over list and put each value in either prePivot or postPivot
             for (int i = 0; i < partition.Count; i++)
             {
-                if (i == pivotVal) continue; // don't do anything with pivot itself
+                if (i == pivotIndex) continue; // don't do anything with pivot itself
 
-                if (partition[i] < partition[pivotVal])
+                if (partition[i] < partition[pivotIndex])
                 {
                     // move left
                     prePivot.Add(partition[i]);
@@ -62,7 +60,7 @@ namespace Sort_opdracht
             // and values bigger are to the right
             // recursively call this function on both prePivot and postPivot so they get fully sorted
             List<int> tmp = new List<int>(Partitioning(prePivot));
-            tmp.Add(partition[pivotVal]); // add pivot between pre and post
+            tmp.Add(partition[pivotIndex]); // add pivot between pre and post
             tmp.AddRange(Partitioning(postPivot));
 
             return tmp;
