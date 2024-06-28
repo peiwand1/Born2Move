ad508217e2af08d61d47f6ce07dac4a808c5dd86
peiwand1 <49898715+peiwand1@users.noreply.github.com>
2024-06-28 17:57:48 +0200

    opdr 5 progress

--- Opdracht_5/BornToMove.DAL/MoveRating.cs
+++ Opdracht_5/BornToMove.DAL/MoveRating.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace BornToMove.DAL
 {
-    public class MoveRating
+    public class MoveRating : IComparable<MoveRating>
     {
         public int id { get; set; }
         public Move move { get; set; }
         public double rating { get; set; }
         public double intensity { get; set; }
+
+        public int CompareTo(MoveRating? other)
+        {
+            if (other == null) return 1;
+
+            if (this.rating > other.rating) return 1;
+            if (this.rating < other.rating) return -1;
+            return 0;
+        }
     }
 }
--- Opdracht_5/Opdracht_5/Program.cs
+++ Opdracht_5/Opdracht_5/Program.cs
@@ -8,16 +8,6 @@ namespace BornToMove
     {
         static void Main(string[] args)
         {
-            List<double> list = new List<double>([3, 4.2345, 5.62346, 6, 7.8678, 1, 2, 3, -4.54, 53, 6, 4, 5, 6, 7, 8, 9]);
-            RotateSort<double> s = new RotateSort<double>();
-
-            list = s.Sort(list);
-
-            foreach (double i in list)
-            {
-                Console.WriteLine(i);
-            }
-
             //MoveContext context = new MoveContext();
             //MoveCrud moveCrud = new MoveCrud(context);
             //MoveRatingCrud moveRatingCrud = new MoveRatingCrud(context);
